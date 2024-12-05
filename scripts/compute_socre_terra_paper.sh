defd252a7f85c8c40dea5664726d934f8e49b187
keishi.ishihara <keishi.ishihara17@gmail.com>
2024-12-05 20:35:04 +0900

    Add shell scripts to reproduce numbers

---
+++ scripts/compute_socre_terra_paper.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python run_benchmark.py \
+    --input_dir generated_videos/terra/geniac2_exp004_epoch_11_framewise \
+    --output_dir results/terra/geniac2_exp004_epoch_11_framewise
